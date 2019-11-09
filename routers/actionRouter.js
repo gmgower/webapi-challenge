bf5bee2517c066c3144792734b1bf65cbe02bdcb
Mada Gower <gmgower@gmail.com>
2019-11-08 20:42:09 -0600

    Setup port & routers.

---
+++ routers/actionRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const actionDB = require('../data/helpers/actionModel.js');
+const projectDB = require('../data/helpers/projectModel.js');
+
+const router = express.Router();
+
+module.exports = router;
---
+++ routers/projectRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const projectDB = require('../data/helpers/projectModel.js');
+const actionDB = require('../data/helpers/actionModel.js');
+
+const router = express.Router();
+
+
+
+
+module.exports = router;
